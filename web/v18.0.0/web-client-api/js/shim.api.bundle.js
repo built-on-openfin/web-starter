e28ef5ada14cec8c9e5c97abae493f636d4ddfc2
johnman <john.mandia@gmail.com>
2024-05-08 07:02:51 +0000

    Deploying to gh-pages from @ built-on-openfin/web-starter@b0e1c51c892b5214da4ab00786ea50901e80b70e 🚀

---
+++ web/v18.0.0/web-client-api/js/shim.api.bundle.js
@@ -0,0 +1,2 @@
+(()=>{"use strict";void 0===window.fin&&(async()=>{try{const t="https://built-on-openfin.github.io/web-starter/web/vnext/web-client-api/js/web.client.api.bundle.js",i=await import(t);await i.init({target:window})}catch(t){console.error("Failed to load the OpenFin API shim. Please note this is an example and you should implement your own approach for production.",t)}})()})();
+//# sourceMappingURL=shim.api.bundle.js.map
