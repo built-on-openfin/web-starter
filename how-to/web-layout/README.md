29ec222036367504843a41562cadc13ef78e0add
John <john.mandia@gmail.com>
2024-05-10 17:15:43 +0100

    Multi Layout sample created by Joe

---
+++ how-to/web-layout/README.md
@@ -0,0 +1,99 @@
+![OpenFin Web Layouts Example](../../assets/openfin-web-starter.png)
+
+> **_:information_source: OpenFin:_** [OpenFin](https://www.openfin.co/) libraries are a commercial product and this repo is for evaluation purposes. Use of the OpenFin npm packages is only granted pursuant to a license from OpenFin. Please [**contact us**](https://www.openfin.co/contact/) if you would like to request a developer evaluation key or to discuss a production license.
+
+# OpenFin Web Layouts
+
+This is a simple example that has a simple provider web page that acts as the main/index page. This page wires up the interop broker and a layout using the [@openfin/core-web](https://www.npmjs.com/package/@openfin/core-web) library.
+
+This page has a very simple layout which is made up of four iframes that inherit the interop settings they should use to connect to the web broker:
+
+- Local - An FDC3 View - This uses the FDC3 API to add a context listener and to broadcast a hardcoded context object.
+- Local - An Interop View - This uses the OpenFin Interop API to add a context listener and to set context using a hardcoded context object.
+- External - An FDC3 Tool used in our workspace platform starters that lets you experiment with context sharing using the FDC3 APIs.
+- External - An Interop Tool used in our workspace platform starters that lets you experiment with context sharing using the OpenFin Interop API.
+
+It also has a left panel which is outside of the OpenFin Layout and represents a platform specific panel which simply uses fdc3 and logs what it receives. This iframe does not inherit interop settings (as it is not part of the OpenFin layout) and uses platform specific settings to connect.
+
+Using Layouts, this demo also shows off a "Swap Layouts" function, which allows someone to quickly jump from one created layout (via a snapshot JSON definition) to another.
+
+[Live Launch Example](https://built-on-openfin.github.io/web-starter/web/v18.0.0/web-layout/platform/provider.html)
+
+![OpenFin Web Multi Layout Example](./docs/web-layout.png)
+
+## Getting Started
+
+1. Install dependencies and do the initial build. Note that these examples assume you are in the sub-directory for the example.
+
+```shell
+npm install
+```
+
+2. Build the example.
+
+```shell
+npm run build
+```
+
+3. Start the test server in a new window.
+
+```shell
+npm run start
+```
+
+4. Launch the sample in your default desktop browser (or copy <http://localhost:6060/platform/provider.html> into your Desktop Browser).
+
+```shell
+npm run client
+```
+
+## Setup Notes
+
+There are a few things to note before trying to use @openfin/core-web:
+
+- If your [tsconfig](./client/tsconfig.json) file is using **node** for moduleResolution it will need to use **Node16** instead as export/imports are defined in the package.json of the @openfin/core-web npm package. This is required for when you try to import @openfin/core-web/iframe-broker.
+- You will need to copy the shared-worker.js file from the [@openfin/core-web](https://www.npmjs.com/package/@openfin/core-web) npm package to your public folder. We have created a [copy-shared-worker.js](./scripts/copy-shared-worker.js) script to do this and it is referenced in the build-client npm command.
+
+## Things to know when using Layouts
+
+### LayoutManagerOverride
+
+When providing more than one layout snapshot to define your view, it is necessary to override the existing layout init function when starting your application.
+
+```javascript
+fin.Platform.Layout.init({ layoutManagerOverride, container: PARENT_CONTAINER });
+```
+
+This allows us to call a function in the LayoutManager class called applyLayoutSnapshot that takes a container element, id string for its name, and a layout object to inject your snapshot layout in the desired container in the view.
+
+By overriding some of the existing layout functions that you might be used to if you've used our Platform API before, we can now pass an larger object (found in /public/layouts/default.layout.fin.json) with multiple layouts, and iterate the creation of them into the DOM.
+
+```javascript
+function layoutManagerOverride(Base: LayoutManagerConstructor):
+ LayoutManagerConstructor {
+   /**
+    * @class LayoutManagerBasic
+    * This implementation is the fundamental override for Multiple Layouts in Web.
+    */
+    return class LayoutManagerBasic extends Base implements LayoutManager {
+     public layoutMapArray: LayoutManagerItem[] = [];
+
+     public layoutContainer = document.querySelector<HTMLElement>("#layout_container");
+
+     /**
+      * Override for applying multiple snapshots.
+      * @param snapshot The layouts object containing the fixed set of available layouts.
+      */
+     public async applyLayoutSnapshot(snapshot: WebLayoutSnapshot): Promise<void> {
+      console.log(`Does this exist? ${Boolean(this.layoutContainer)}`);
+      if (this.layoutContainer !== null && this.layoutContainer !== undefined) {
+        await Promise.all(Object.entries(snapshot.layouts).map(async ([layoutName, layout], i) =>
+          createLayout(fin, layoutName, layout, i))
+        );
+
+       console.log("Layouts loaded");
+      }
+     }
+     };
+   };
+```
---
+++ how-to/web-layout/client/src/provider.ts
@@ -0,0 +1,204 @@
+import type OpenFin from "@openfin/core";
+import { type WebLayoutSnapshot, connect } from "@openfin/core-web";
+import { getDefaultLayout, getSettings } from "./platform/settings";
+import type { LayoutManager, LayoutManagerConstructor, LayoutManagerItem } from "./shapes/layout-shapes";
+import type { Settings } from "./shapes/setting-shapes";
+
+let PARENT_CONTAINER: HTMLElement | null;
+
+/**
+ * Sets up panels if supported.
+ * @param settings The settings to use.
+ */
+function setupPanels(settings: Settings): void {
+	if (settings?.platform?.layout?.panels?.left) {
+		const leftPanel = settings.platform.layout.panels.left;
+		const leftPanelFrameContainer = document.querySelector<HTMLElement>(`#${leftPanel.frameContainerId}`);
+		const leftPanelFrame = document.querySelector<HTMLIFrameElement>(`#${leftPanel.frameId}`);
+		if (leftPanelFrameContainer === null) {
+			console.error(
+				`Please ensure the document has an element with the following id #${leftPanel.frameContainerId} containing an iframe with an id of #${leftPanel.frameId} so that the layout can be applied.`
+			);
+			return;
+		}
+		if (leftPanelFrame === null) {
+			console.error(
+				`Please ensure the document has an iframe with the following id #${leftPanel.frameId} so that the layout can be applied.`
+			);
+			return;
+		}
+		leftPanelFrameContainer.classList.remove("hidden");
+		leftPanelFrame.src = leftPanel.url;
+		console.log(`Panel ${leftPanel.frameId} has been setup with the url ${leftPanel.url}`);
+	} else {
+		console.log("No panels require setup.");
+	}
+}
+
+/**
+ * Attach listeners to elements.
+ */
+async function attachListeners(): Promise<void> {
+	const swapButton = document.querySelector<HTMLButtonElement>("#swap-layouts");
+	swapButton?.addEventListener("click", async () => {
+		await swapLayout();
+	});
+}
+
+/**
+ * A Create function for layouts.
+ * @param fin the fin object.
+ * @param layoutName A string for the layout name.
+ * @param layout LayoutOptions
+ * @param order which position to display in
+ */
+async function createLayout(
+	fin: OpenFin.Fin<OpenFin.EntityType>,
+	layoutName: string,
+	layout: OpenFin.LayoutOptions,
+	order: number
+): Promise<void> {
+	// Create a new div container for the layout.
+	const container = document.createElement("div");
+	container.id = layoutName;
+	container.className = "col fill";
+	container.style.display = order === 0 ? "block" : "none";
+	PARENT_CONTAINER?.append(container);
+
+	// Normally you can use state here, but just tracking the order of layouts in localStorage.
+	const currentOrder = window.localStorage.getItem("order");
+	if (!currentOrder) {
+		window.localStorage.setItem("order", "");
+	}
+	let newOrder = "";
+	if (order === 0) {
+		newOrder = layoutName;
+	} else {
+		newOrder = currentOrder ? currentOrder.concat(",", layoutName) : "";
+	}
+	window.localStorage.setItem("order", newOrder);
+	// Finally, call the Layout.create() function to apply the snapshot layout to the div we just created.
+	await fin.Platform.Layout.create({ layoutName, layout, container });
+}
+
+/**
+ * MakeOverride assists in loading the Fin object before the applyLayoutSnapshot Manager call.
+ * @param fin the fin object.
+ * @param layoutContainerId the layout container id.
+ * @returns a function call.
+ */
+function makeOverride(fin: OpenFin.Fin<OpenFin.EntityType>, layoutContainerId: string) {
+	return function layoutManagerOverride(Base: LayoutManagerConstructor): LayoutManagerConstructor {
+		/**
+		 * @class LayoutManagerBasic
+		 * This implementation is the fundamental override for Multiple Layouts in Web.
+		 */
+		return class LayoutManagerBasic extends Base implements LayoutManager {
+			public layoutMapArray: LayoutManagerItem[] = [];
+
+			public layoutContainer = document.querySelector<HTMLElement>(`#${layoutContainerId}`);
+
+			/**
+			 * Override for applying multiple snapshots.
+			 * @param snapshot The layouts object containing the fixed set of available layouts.
+			 */
+			public async applyLayoutSnapshot(snapshot: WebLayoutSnapshot): Promise<void> {
+				console.log(`Does this exist? ${Boolean(this.layoutContainer)}`);
+				if (this.layoutContainer !== null && this.layoutContainer !== undefined) {
+					setTimeout(
+						() =>
+							Object.entries(snapshot.layouts).map(async ([layoutName, layout], i) =>
+								createLayout(fin, layoutName, layout, i)
+							),
+						1000
+					);
+					console.log("Layouts loaded");
+				}
+			}
+		};
+	};
+}
+
+/**
+ * Returns a layout from the settings with a provided name.
+ * @returns The default layout from the settings.
+ */
+export async function swapLayout(): Promise<void> {
+	// Get that order of created div ids from storage, or state, or wherever you want to save them.
+	const currentOrder = window.localStorage.getItem("order");
+	const layouts = currentOrder?.split(",");
+	// This is a simple swap between two, but you can do this anyway you like.
+	const firstLayout = document.querySelector<HTMLElement>(`#${layouts ? layouts[0] : null}`);
+	const secondLayout = document.querySelector<HTMLElement>(`#${layouts ? layouts[1] : null}`);
+	if (firstLayout && secondLayout) {
+		if (secondLayout.style.display === "block") {
+			firstLayout.style.display = "block";
+			secondLayout.style.display = "none";
+		} else {
+			firstLayout.style.display = "none";
+			secondLayout.style.display = "block";
+		}
+	}
+}
+
+/**
+ * Initializes the OpenFin Web Broker connection.
+ */
+async function init(): Promise<void> {
+	// Get the required settings
+	const settings = await getSettings();
+	// Get the default layout
+	const layoutSnapshot = await getDefaultLayout();
+
+	if (settings === undefined || layoutSnapshot === undefined) {
+		console.error(
+			"Unable to run the sample as we have been unable to load the web manifest and it's settings from the currently running html page. Please ensure that the web manifest is being served and that it contains the custom_settings section."
+		);
+		return;
+	}
+
+	// Get the dom element that should host the layout - This will be the top element holding the children iframes.
+	PARENT_CONTAINER = document.querySelector<HTMLElement>(`#${settings.platform.layout.layoutContainerId}`);
+
+	if (PARENT_CONTAINER === null) {
+		console.error(
+			`Please ensure the document has an element with the following id #${settings.platform.layout.layoutContainerId} so that the web-layout can be applied.`
+		);
+		return;
+	}
+
+	// Connect to the OpenFin Web Broker and pass the default layout.
+	// It is good practice to specify providerId even if content is explicitly specifying it for cases where
+	// this provider uses our layout system and content uses inheritance. currentContextGroup
+	// is useful for defaulting any client that uses inheritance through our layout system.
+	const fin = await connect({
+		options: {
+			brokerUrl: settings.platform.interop.brokerUrl,
+			interopConfig: {
+				providerId: settings.platform.interop.providerId,
+				currentContextGroup: settings.platform.interop.defaultContextGroup
+			}
+		},
+		connectionInheritance: "enabled",
+		platform: { layoutSnapshot }
+	});
+
+	if (fin) {
+		const layoutManagerOverride = makeOverride(fin, settings.platform.layout.layoutContainerId);
+		// You may now use the `fin` object to initialize the broker and the layout.
+		await fin.Interop.init(settings.platform.interop.providerId);
+		// Show the main container and hide the loading container
+		// initialize the layout and pass it the dom element to bind to
+		await fin.Platform.Layout.init({ layoutManagerOverride, container: PARENT_CONTAINER });
+		// setup panels not that everything has been initialized
+		await attachListeners();
+		setupPanels(settings);
+	}
+}
+
+init()
+	.then(() => {
+		console.log("Connected to the OpenFin Web Broker and layout has been applied.");
+		return true;
+	})
+	.catch((err) => console.error(err));
