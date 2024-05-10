97b9bc2bbeeec924c8d257446ca67ab85027cbad
John <john.mandia@gmail.com>
2024-05-10 17:25:06 +0100

    Multi Layout sample created by Joe (#7)
    * Multi Layout sample created by Joe

---
+++ how-to/web-layout/client/src/content/fdc3-view.ts
@@ -0,0 +1,80 @@
+import type { Context } from "@finos/fdc3";
+import { init } from "../platform/api";
+
+window.addEventListener("DOMContentLoaded", async () => {
+	await init(true);
+	await initializeDOM();
+});
+
+/**
+ * Broadcasts a context using FDC3.
+ */
+async function broadcastContext(): Promise<void> {
+	const contextType = "fdc3.instrument";
+	const contextName = "Apple";
+	const idData = {
+		ticker: "AAPL"
+	};
+	const context = {
+		type: contextType,
+		name: contextName,
+		id: idData
+	};
+	if (window.fdc3) {
+		await window.fdc3.broadcast(context);
+		console.log(`Broadcasted context: ${contextType} - ${contextName}`);
+	} else {
+		window.addEventListener("fdc3Ready", async () => {
+			await window.fdc3.broadcast(context);
+			console.log(`Broadcasted context: ${contextType} - ${contextName}`);
+		});
+	}
+}
+
+/**
+ * Adds an FDC3 context listener to the window.
+ */
+async function addFDC3Listener(): Promise<void> {
+	if (window.fdc3) {
+		await window.fdc3.addContextListener(null, (context) => {
+			updateDOMElements(context);
+		});
+	} else {
+		window.addEventListener("fdc3Ready", async () => {
+			await window.fdc3.addContextListener(null, (context) => {
+				updateDOMElements(context);
+			});
+		});
+	}
+}
+
+/**
+ * Updates the DOM elements with the provided context.
+ * @param context The context to update the DOM elements with.
+ */
+function updateDOMElements(context: Context): void {
+	const contextTypeSpan = document.querySelector<HTMLSpanElement>("#contextType");
+	const contextNameSpan = document.querySelector<HTMLSpanElement>("#contextName");
+	const contextBodyPre = document.querySelector<HTMLPreElement>("#contextBody");
+
+	if (contextTypeSpan !== null && contextNameSpan !== null && contextBodyPre !== null) {
+		contextTypeSpan.textContent = context.type;
+		contextNameSpan.textContent = context.name ?? "No name provided.";
+		contextBodyPre.textContent = JSON.stringify(context, null, 2);
+	}
+}
+
+/**
+ * Initialize the DOM elements.
+ */
+async function initializeDOM(): Promise<void> {
+	const broadcastButton = document.querySelector<HTMLButtonElement>("#broadcast");
+
+	if (broadcastButton !== null) {
+		broadcastButton.addEventListener("click", async () => {
+			await broadcastContext();
+		});
+	}
+
+	await addFDC3Listener();
+}
---
+++ how-to/web-layout/client/src/platform/settings.ts
@@ -0,0 +1,69 @@
+import type { WebLayoutSnapshot } from "@openfin/core-web";
+import type { Settings } from "../shapes/setting-shapes";
+
+/**
+ * Fetches the settings for the application.
+ * @returns The settings for the application.
+ */
+export async function getSettings(): Promise<Settings | undefined> {
+	const settings = await getManifestSettings();
+	if (settings === undefined) {
+		console.error(
+			"Unable to run the example as settings are required and we fetch them from the link web manifest from the html page that is being served. It should exist in the customSettings section of the web manifest."
+		);
+	}
+	return settings;
+}
+
+/**
+ * Returns a default layout from the settings if provided.
+ * @returns The default layout from the settings.
+ */
+export async function getDefaultLayout(): Promise<WebLayoutSnapshot | undefined> {
+	const settings = await getSettings();
+	if (settings?.platform?.layout?.defaultLayout === undefined) {
+		console.error(
+			"Unable to run the example as without a layout being defined. Please ensure that settings have been provided in the web manifest."
+		);
+		return;
+	}
+	if (typeof settings.platform.layout.defaultLayout === "string") {
+		const layoutResponse = await fetch(settings.platform.layout.defaultLayout);
+		const layoutJson = (await layoutResponse.json()) as WebLayoutSnapshot;
+		return layoutJson;
+	}
+	return settings.platform.layout.defaultLayout;
+}
+
+/**
+ * Returns a default layout from the settings if provided.
+ * @returns The default layout from the settings.
+ */
+export async function getSecondLayout(): Promise<WebLayoutSnapshot | undefined> {
+	const settings = await getSettings();
+	if (settings?.platform?.layout?.secondLayout === undefined) {
+		console.error(
+			"Unable to run the example as without a layout being defined. Please ensure that settings have been provided in the web manifest."
+		);
+		return;
+	}
+	if (typeof settings.platform.layout.secondLayout === "string") {
+		const layoutResponse = await fetch(settings.platform.layout.secondLayout);
+		const layoutJson = (await layoutResponse.json()) as WebLayoutSnapshot;
+		return layoutJson;
+	}
+	return settings.platform.layout.secondLayout;
+}
+/**
+ * Returns the settings from the manifest file.
+ * @returns customSettings for this example
+ */
+async function getManifestSettings(): Promise<Settings | undefined> {
+	// Get the manifest link
+	const link = document.querySelector<HTMLLinkElement>('link[rel="manifest"]');
+	if (link !== null) {
+		const manifestResponse = await fetch(link.href);
+		const manifestJson = (await manifestResponse.json()) as { custom_settings: Settings };
+		return manifestJson.custom_settings;
+	}
+}
