d6870ca7c89c159a20e44fcdb9560b5b499df852
John <john.mandia@gmail.com>
2024-06-21 18:17:30 +0100

    Update for v18.0.0 path and linting.

--- how-to/web-client-api/client/src/shim.ts
+++ how-to/web-client-api/client/src/shim.ts
@@ -3,7 +3,7 @@ if (window.fin === undefined) {
 	(async () => {
 		try {
 			const url =
-				"https://built-on-openfin.github.io/web-starter/web/vnext/web-client-api/js/web.client.api.bundle.js";
+				"https://built-on-openfin.github.io/web-starter/web/v18.0.0/web-client-api/js/web.client.api.bundle.js";
 			const webClient = await import(/* webpackIgnore: true */ url);
 			await webClient.getAPI({ target: window });
 		} catch (error) {
