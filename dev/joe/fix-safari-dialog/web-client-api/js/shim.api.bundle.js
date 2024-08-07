70f9e4ce1283b49bcc11084e1cba364053658c73
joeransegnola <129192453+joeransegnola@users.noreply.github.com>
2024-08-07 12:35:34 +0000

    Deploying to gh-pages from @ built-on-openfin/web-starter@9c84aacab209f2e909bd57bf12c960fed3aea454 🚀

---
+++ dev/joe/fix-safari-dialog/web-client-api/js/shim.api.bundle.js
@@ -0,0 +1,2 @@
+(()=>{"use strict";void 0===window.fin&&(async()=>{try{const t="https://built-on-openfin.github.io/web-starter/web/v19.0.0/web-client-api/js/web.client.api.bundle.js",e=await import(t);await e.getAPI({target:window})}catch(t){console.error("Failed to load the OpenFin API shim. Please note this is an example and you should implement your own approach for production.",t)}})()})();
+//# sourceMappingURL=shim.api.bundle.js.map
