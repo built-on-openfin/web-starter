bde2264d9e71ea032bda0464c98e8e4400b7c8cd
johnman <john.mandia@gmail.com>
2024-11-15 10:20:59 +0000

    Deploying to gh-pages from @ built-on-openfin/web-starter@770a8e5d0161359fd8fe2e1783459eb63798b6a2 🚀

--- main/web-interop-support-context-and-intents/js/settings-dialog.bundle.js
+++ main/web-interop-support-context-and-intents/js/settings-dialog.bundle.js
@@ -1,7 +1,7 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	"use strict";
 var __webpack_exports__ = {};
-// This entry need to be wrapped in an IIFE because it uses a non-standard name for the exports (exports).
+// This entry needs to be wrapped in an IIFE because it uses a non-standard name for the exports (exports).
 (() => {
 var exports = __webpack_exports__;
 /*!***********************************************!*\
