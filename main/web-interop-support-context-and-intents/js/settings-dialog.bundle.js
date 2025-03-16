c0001a044a69c3ea3b5b2641c2a516e281b6f5ff
raycashmore <raycashmore@users.noreply.github.com>
2025-03-16 23:53:20 +0000

    Deploying to gh-pages from @ built-on-openfin/web-starter@69a505e325abb28b4794c5239786dc1928ba262b 🚀

--- main/web-interop-support-context-and-intents/js/settings-dialog.bundle.js
+++ main/web-interop-support-context-and-intents/js/settings-dialog.bundle.js
@@ -62,8 +62,8 @@ async function init() {
         title.value = settings?.platform?.ui?.title;
         subTitle.value = settings?.platform?.ui?.subTitle;
         logo.value = settings?.platform?.ui?.logo;
-        username.value = settings?.platform.cloudInterop?.connectParams?.basicAuthenticationParameters?.username;
-        password.value = settings?.platform.cloudInterop?.connectParams?.basicAuthenticationParameters?.password;
+        username.value = settings?.platform.cloudInterop?.connectParams?.basicAuthenticationParameters?.username ?? "";
+        password.value = settings?.platform.cloudInterop?.connectParams?.basicAuthenticationParameters?.password ?? "";
         platformId.value = settings?.platform.cloudInterop?.connectParams?.platformId;
         cloudUrl.value = settings?.platform.cloudInterop?.connectParams?.url;
         sourceId.value = settings?.platform.cloudInterop?.connectParams.sourceId ?? "";
@@ -78,10 +78,10 @@ async function init() {
         appliedSettings.platform.ui.title = title.value;
         appliedSettings.platform.ui.subTitle = subTitle.value;
         appliedSettings.platform.ui.logo = logo.value;
-        appliedSettings.platform.cloudInterop.connectParams.basicAuthenticationParameters.username =
-            username.value;
-        appliedSettings.platform.cloudInterop.connectParams.basicAuthenticationParameters.password =
-            password.value;
+        if (appliedSettings.platform?.cloudInterop?.connectParams?.basicAuthenticationParameters) {
+            appliedSettings.platform.cloudInterop.connectParams.basicAuthenticationParameters.username = username.value;
+            appliedSettings.platform.cloudInterop.connectParams.basicAuthenticationParameters.password = password.value;
+        }
         appliedSettings.platform.cloudInterop.connectParams.platformId = platformId.value;
         appliedSettings.platform.cloudInterop.connectParams.url = cloudUrl.value;
         appliedSettings.platform.cloudInterop.connectParams.sourceId = sourceId.value;
