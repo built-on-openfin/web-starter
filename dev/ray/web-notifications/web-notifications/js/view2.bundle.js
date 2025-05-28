798e6626c98938368f129956008a0a0535f49c55
raycashmore <raycashmore@users.noreply.github.com>
2025-05-28 13:27:33 +0000

    Deploying to gh-pages from @ built-on-openfin/web-starter@b73024b896cac2c580e8b596bdb763c7d5c5da85 🚀

---
+++ dev/ray/web-notifications/web-notifications/js/view2.bundle.js
@@ -0,0 +1,16 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+// This entry needs to be wrapped in an IIFE because it uses a non-standard name for the exports (exports).
+(() => {
+var exports = __webpack_exports__;
+/*!*************************************!*\
+  !*** ./client/src/content/view2.ts ***!
+  \*************************************/
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+
+})();
+
+/******/ })()
+;
