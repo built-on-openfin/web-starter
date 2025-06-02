e19d1088199a0499941f0081750e2ca4f48d967f
Ray Cashmore <raycashmore@users.noreply.github.com>
2025-06-02 23:55:23 +1000

    Web notifications starter (#24)
    * Add web-notifications starter
    
    * Show simple notification
    
    * Cleanup
    
    * Update readme

---
+++ how-to/web-notifications/client/src/platform/iframe-broker.ts
@@ -0,0 +1,27 @@
+import { init as initBrokerConnection } from "@openfin/core-web/iframe-broker";
+import { getSettings } from "./settings";
+
+/**
+ * Initializes the OpenFin Web Broker connection.
+ * @returns A promise that resolves when the connection is established.
+ */
+async function init(): Promise<void> {
+	const settings = await getSettings();
+	if (settings === undefined) {
+		console.error(
+			"Unable to run the sample as we have been unable to load the web manifest and it's settings from the currently running html page. Please ensure that the web manifest is being served and that it contains the custom_settings section."
+		);
+		return;
+	}
+	return initBrokerConnection({
+		sharedWorkerUrl: settings.platform.interop.sharedWorkerUrl,
+		logLevel: "info"
+	});
+}
+
+init()
+	.then(() => {
+		console.log("Connected to the OpenFin IFrame Web Broker.");
+		return true;
+	})
+	.catch((err) => console.error(err));
